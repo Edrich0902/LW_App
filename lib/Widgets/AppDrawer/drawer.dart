fc63e8213a1f7d431b70eee5158ebd17e13ebaa5
Edrich Barnard <edrichbarnard@gmail.com>
2023-04-08 17:41:07 +0200

    feature: show user profile in app drawer

--- lib/Widgets/AppDrawer/drawer.dart
+++ lib/Widgets/AppDrawer/drawer.dart
@@ -4,9 +4,23 @@ import 'package:lw_app/Blocs/Auth/auth_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lw_app/Screens/Profile/profile.dart';
 import 'package:lw_app/Screens/Dashboard/dashboard.dart';
+import 'package:lw_app/Blocs/User/user_bloc.dart';
 
-class AppDrawer extends StatelessWidget {
+class AppDrawer extends StatefulWidget {
+  const AppDrawer({super.key});
+
+  @override
+  State<AppDrawer> createState() => _AppDrawerState();
+}
+
+class _AppDrawerState extends State<AppDrawer> {
   SB.User? user = SB.Supabase.instance.client.auth.currentUser;
+
+  @override
+  void initState() {
+    context.read<UserBloc>().add(LoadUser());
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -62,20 +76,36 @@ class AppDrawer extends StatelessWidget {
       ),
       child: InkWell(
         onTap: onTap,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CircleAvatar(
-              radius: 50,
-              //TODO: update with actual user image and add placeholder
-              backgroundImage: NetworkImage(
-                  "https://yt3.googleusercontent.com/ytc/AL5GRJUbsh7ILjzuEQAZTot_kkV2GohZR75CjoWM9NSI9Q=s900-c-k-c0x00ffffff-no-rj"),
-            ),
-            SizedBox(height: 8),
-            Text(user?.email ?? ""),
-          ],
-        ),
+        child: BlocBuilder<UserBloc, UserState>(builder: (context, state) {
+          if (state is UserLoading) {
+            return CircularProgressIndicator.adaptive();
+          } else if (state is UserSuccess) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 45,
+                  //TODO: update with actual user image and add placeholder
+                  backgroundImage: NetworkImage(
+                      "https://yt3.googleusercontent.com/ytc/AL5GRJUbsh7ILjzuEQAZTot_kkV2GohZR75CjoWM9NSI9Q=s900-c-k-c0x00ffffff-no-rj"),
+                ),
+                SizedBox(height: 8),
+                Text("${state.user.firstName} ${state.user.lastName}" ?? ""),
+                SizedBox(height: 8),
+                Text(
+                  user?.email ?? "",
+                  style: TextStyle(
+                    fontSize: 11.0,
+                    fontWeight: FontWeight.w200,
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return Text('Could not load Profile');
+          }
+        }),
       ),
     );
   }
