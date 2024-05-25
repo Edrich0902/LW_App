4f6ae40a2baeb44511a7dcf2f15e73b01ed1a47f
Edrich Barnard <edrichbarnard@gmail.com>
2024-05-25 15:54:07 +0200

    feature: rework and enhance bottom nav with pageview

--- lib/Screens/Container/container.dart
+++ lib/Screens/Container/container.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lw_app/Widgets/AppDrawer/bottom_nav.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 // Screens
 import 'package:lw_app/Screens/Dashboard/dashboard.dart';
@@ -14,7 +14,7 @@ class ContainerPage extends StatefulWidget {
 }
 
 class _ContainerPageState extends State<ContainerPage> {
-
+  PageController _pageViewController = PageController();
   int _selectedIndex = 0;
 
   @override
@@ -22,25 +22,83 @@ class _ContainerPageState extends State<ContainerPage> {
     super.initState();
   }
 
-  void updateIndex(int index) {
+  void itemChanged(int index) {
+    setState(() {
+      _selectedIndex = index;
+      _pageViewController.animateToPage(index, duration: Duration(milliseconds: 300), curve: Curves.ease);
+    });
+  }
+
+  void pageChanged(int index) {
     setState(() {
       _selectedIndex = index;
     });
   }
 
-  // TODO: add other bottom nav pages here
-  static const List<Widget> _screens = <Widget>[
-    DashPage(),
-    NotesPage(),
-    ProfilePage(),
-    ProfilePage(),
-  ];
-
   @override
   Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+
     return Scaffold(
-      body: _screens[_selectedIndex],
-      bottomNavigationBar: AppBottomNav(handleScreenIndex: updateIndex),
+      // body: _screens[_selectedIndex],
+      body: PageView(
+        controller: _pageViewController,
+        children: <Widget>[
+          // TODO: add other pages to the page view here
+          DashPage(),
+          NotesPage(),
+          ProfilePage(),
+          ProfilePage(),
+        ],
+        onPageChanged: (index) => pageChanged(index),
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: theme.primaryColor,
+          border: Border.all(
+            color: theme.primaryColor,
+          ),
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(24.0),
+            topLeft: Radius.circular(24.0),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(15, 15, 15, 20),
+          child: GNav(
+            selectedIndex: _selectedIndex,
+            onTabChange: (index) => itemChanged(index),
+            tabs: [
+              // TODO: add buttons to bottom nav pages here
+              GButton(
+                icon: Icons.home,
+                text: 'Home',
+              ),
+              GButton(
+                icon: Icons.notes,
+                text: 'Notes',
+              ),
+              GButton(
+                icon: Icons.info_outline,
+                text: 'More Info',
+              ),
+              GButton(
+                icon: Icons.person,
+                text: 'Profile',
+              ),
+            ],
+            gap: 8,
+            padding: EdgeInsets.all(16.0),
+            curve: Curves.easeInOut,
+            duration: Duration(milliseconds: 300),
+            haptic: true,
+            tabBorderRadius: 24.0,
+            tabBackgroundColor: Colors.white,
+            activeColor: theme.primaryColor,
+            color: Colors.white,
+          ),
+        ),
+      ),
     );
   }
 }
